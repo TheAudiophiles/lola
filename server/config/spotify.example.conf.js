11cc3c3aea06cbb4f63676a94f540e7faac7a2d4
BLuEScioN <barnettn26@gmail.com>
2017-02-15 12:16:23 -0600

    minimal unstaged changes before rebase

--- server/config/spotify.example.conf.js
+++ server/config/spotify.example.conf.js
@@ -1,4 +1,4 @@
-const CLIENT_ID     = 'client id here';
-const CLIENT_SECRET = 'secret here';
+const CLIENT_ID     = '163082aadae6480faafce6ffea3387db';
+const CLIENT_SECRET = '8b058e92c64c4f8ebe614010e5afa0ba';
 
 module.exports = { clientID: CLIENT_ID, clientSecret: CLIENT_SECRET };
