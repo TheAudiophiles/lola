5b6996cd028a2670d11673f8287d7aad2b191318
BLuEScioN <barnettn26@gmail.com>
2017-03-01 15:04:11 -0600

    fixed indentation in controllers fo real doe

--- server/controllers/LibraryController.js
+++ server/controllers/LibraryController.js
@@ -8,56 +8,56 @@ class LibraryController {
   // findSongsByArtist
   // findSongsByAlbum
 
-	createLibrary(user, done) {
-		this.library = new Library({
-			user: user,
-			songs: []
-		});
-		this.library.save(err => {
-			if (err) {
-				done(err);
-			} else {
-				done();
-			}
-		});
-	}
+  createLibrary(user, done) {
+    this.library = new Library({
+      user: user,
+      songs: []
+    });
+    this.library.save(err => {
+      if (err) {
+        done(err);
+      } else {
+        done();
+      }
+    });
+  }
 
   findLibrary(user, done) {
     Library.findOne({ user }, (err, library) => {
-			if (err) {
-				done(err);
-			} else {
-				this.library = library;
-				done(null, library);
-			}
-		});
+      if (err) {
+        done(err);
+      } else {
+        this.library = library;
+        done(null, library);
+      }
+    });
   }
 
   addSong(newSong, done) {
-		console.log('LIBRARY CONTROLLER - this.library before adding song:', this.library);
+    console.log('LIBRARY CONTROLLER - this.library before adding song:', this.library);
     let exists = false;
-		for (let song of this.library.songs) {
-			if (song.title === newSong.title) {
-				exists = true;
-				break;
-			}
-		}
-		if (!exists) {
-			console.log(newSong.title, 'hasn\'t been added to the library yet');
-			this.library.songs.push(newSong);
-			this.library.save(err => {
-				if (err) {
-					done(err);
-				} else {
-					done();
-				}
-			});
-		} else {
-		// 	should let the user know the song is already in the db!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		console.log('LIBRARY CONTROLLER - this.library after adding song:', this.library);
-    done();
+    for (let song of this.library.songs) {
+      if (song.title === newSong.title) {
+        exists = true;
+        break;
+      }
     }
-	}
+    if (!exists) {
+      console.log(newSong.title, 'hasn\'t been added to the library yet');
+      this.library.songs.push(newSong);
+      this.library.save(err => {
+        if (err) {
+          done(err);
+        } else {
+          done();
+        }
+      });
+    } else {
+      // 	should let the user know the song is already in the db!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      console.log('LIBRARY CONTROLLER - this.library after adding song:', this.library);
+      done();
+    }
+  }
 }
 
 module.exports = LibraryController;
--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,46 +1,46 @@
 const User = require('../models/UserModel');
 
 class UserController {
-	constructor() {
-		this.user = {};
-	}
+  constructor() {
+    this.user = {};
+  }
 
-	findById(id, done) {
-	  User.findById(id, (err, user) => {
-	    done(err, user);
-	  });
-	}
+  findById(id, done) {
+    User.findById(id, (err, user) => {
+      done(err, user);
+    });
+  }
 
-	findUser(username, done) {
-		User.findOne({ username }, (err, user) => {
-			if (err) {
-				done(err);
-			} else {
-				this.user = user;
-				done(null, user);
-			}
-		});
-	}
+  findUser(username, done) {
+    User.findOne({ username }, (err, user) => {
+      if (err) {
+        done(err);
+      } else {
+        this.user = user;
+        done(null, user);
+      }
+    });
+  }
 
-	createUser(data, done) {
-		this.user = new User({
-			username: data.id,
-			name: data.display_name || '',
-			email: data.email || ''
-		});
-		this.user.save(err => {
-			if (err) {
-				done(err);
-			} else {
-				done();
-			}
-		});
-	}
+  createUser(data, done) {
+    this.user = new User({
+      username: data.id,
+      name: data.display_name || '',
+      email: data.email || ''
+    });
+    this.user.save(err => {
+      if (err) {
+        done(err);
+      } else {
+        done();
+      }
+    });
+  }
 
-	getUserId() {
-		// console.log('GETTING USERID - this.user._id:', this.user._id);
-		return this.user._id;
-	}
+  getUserId() {
+    // console.log('GETTING USERID - this.user._id:', this.user._id);
+    return this.user._id;
+  }
 
 }
 
