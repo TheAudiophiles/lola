452c4cd4037a463ce06980c9904540aef7b0e560
Rachel <racheldepriest1@gmail.com>
2017-03-01 17:10:44 -0600

    Added all command information

--- src/app/containers/command_info/CommandInfo.jsx
+++ src/app/containers/command_info/CommandInfo.jsx
@@ -32,17 +32,17 @@ export default class CommandInfo extends Component {
         >
           <Modal.Header closeButton>
             <Modal.Title id="contained-modal-title" style={{color: "black"}}>Lola's Commands</Modal.Title>
-            
           </Modal.Header>
           <Modal.Body> 
-            <div style={{color: "black"}}>Play Song by Lyrics</div>
-            <div style={{color: "black"}}>Play Song by Artist</div>
-            <div style={{color: "black"}}>Play Song by Track</div> 
-            <div style={{color: "black"}}>Pause Song</div>
-            <div style={{color: "black"}}>Previous Song</div>
-            <div style={{color: "black"}}>Next Song </div>
-            <div style={{color: "black"}}>Mute</div>
-            <div style={{color: "black"}}>Unmute</div>
+            <div style={{color: "black"}}>To play song by lyrics say: Song contains 'hello it's me'  </div>
+            <div style={{color: "black"}}>To play song by an artist say: Play 'Hello' by Adele </div> 
+            <div style={{color: "black"}}>To pause a song say: pause or pause song</div>
+            <div style={{color: "black"}}>To play the previous song in your queue say: previous song</div>
+            <div style={{color: "black"}}>To play the next song in your queue say: next song</div>
+            <div style={{color: "black"}}>To mute the song say: mute</div>
+            <div style={{color: "black"}}>To unmute the song say: unmute</div>
+            <div style={{color: "black"}}>To increase the volume say: increase volume</div>
+            <div style={{color: "black"}}>To decrease the volume say: unmute</div>
           </Modal.Body>
         </Modal>
       </div>
