ab4ca8ae5b328c72b50a4f78ec9ce646d4b9ae88
BLuEScioN <barnettn26@gmail.com>
2017-03-01 17:13:01 -0600

    rendering queue and library containers appropriately

--- src/app/containers/qlToggle/QLToggle.jsx
+++ src/app/containers/qlToggle/QLToggle.jsx
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Button } from 'rebass';
 import { toggleQueue, toggleLibrary } from '../../actions';
+import Queue from '../queue/Queue';
+import Library from '../library/Library';
 
 class QLToggle extends Component {
 
@@ -15,11 +17,19 @@ class QLToggle extends Component {
   }
 
   render() {
+    let component;
+    this.props.queueOn ? component = <Queue/> : component = <Library/>;
+
     return (
       <div>
+      <div className='buttons'>
         <Button onClick={this.toggleQueueClickHandler.bind(this)} disabled={this.props.queueOn}>Queue</Button>
         <Button onClick={this.toggleLibraryClickHandler.bind(this)} disabled={this.props.libraryOn}>Toggle</Button>
       </div>
+      <div className='toggle'>
+        {component}
+      </div>
+    </div>
     );
   }
 }
