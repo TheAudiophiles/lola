2d608735f99ada66c41d7e41be8c07610869105f
Rachel <racheldepriest1@gmail.com>
2017-03-02 11:52:32 -0600

    Lola is listening message appears on screen

--- src/app/components/speech/Speech.jsx
+++ src/app/components/speech/Speech.jsx
@@ -5,6 +5,17 @@ import { Button } from 'rebass';
 
 import mic from './mic-icon.png';
 
+class Message extends Component {
+  constructor(){
+    super()
+  }
+  render(){
+    return (
+      <div>Lola is listening</div>
+    )
+  }
+}
+
 export default class Speech extends Component {
   constructor(props) {
     super(props);
@@ -13,19 +24,28 @@ export default class Speech extends Component {
     const { artyom, commands } = this.speech;
 
     artyom.addCommands(commands);
+
+    this.state = {
+      showClicked: false
+    }
   }
 
   clickHandler = (e) => {
     e.preventDefault();
 
     this.speech.init();
-  }
 
+    this.setState({showClicked: !this.state.showClicked})
+  }
+ 
   render() {
     return (
-      <a href="#" onClick={this.clickHandler}>
-        <img src={mic} />
-      </a>
+      <div>
+        <a href="#" onClick={this.clickHandler.bind(this)}>
+          <img src={mic} />
+        </a>
+        {this.state.showClicked && < Message />}
+      </div>
     );
   }
 }
