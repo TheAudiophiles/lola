5930fb4a4081d800c73c1203e735d63f51537e3f
Daniel Olita <danielolita1@gmail.com>
2017-03-03 15:23:34 -0600

    Fixed url in spotify config

--- server/config/spotify.example.conf.js
+++ server/config/spotify.example.conf.js
@@ -1,6 +1,6 @@
 const CLIENT_ID = 'client id here';
 const CLIENT_SECRET = 'client secret here';
-const CALLBACK_URL = 'https://localhost:3000/callback';
+const CALLBACK_URL = 'http://localhost:3000/callback';
 
 module.exports = {
   clientID: CLIENT_ID,
