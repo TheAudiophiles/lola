1e97c4fbe43fed921f8ed93f86d7894951da0451
Daniel Olita <danielolita1@gmail.com>
2017-03-03 12:34:29 -0600

    Moved spotify callback url to spotify config file

--- server/config/spotify.example.conf.js
+++ server/config/spotify.example.conf.js
@@ -1,7 +1,9 @@
 const CLIENT_ID = 'client id here';
 const CLIENT_SECRET = 'client secret here';
+const CALLBACK_URL = 'https://localhost:3000/callback';
 
 module.exports = {
   clientID: CLIENT_ID,
-  clientSecret: CLIENT_SECRET
+  clientSecret: CLIENT_SECRET,
+  callbackUrl: CALLBACK_URL
 };
